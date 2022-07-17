6fc09cdcf5cb316bbd6cc9747619723fb879ea20
macm <mac.mitsch@comcast.net>
2022-07-17 17:26:43 -0500

    cant get it to post trying to redo project

---
+++ db/store.js
@@ -0,0 +1,41 @@
+const util = require("util");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const { parse } = require("path");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Storage{
+    read(){
+        return readFileAsync("db/db.json", "utf8")
+    }
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note))
+    }
+    addNote(note){
+        const { title, text } = note
+        if(!title|| !text){
+            throw new Error("error please fill title and text.")
+        }
+        const newNote = {title,text,id:uuidv4()}
+
+        return this.getNotes()
+        .then(notes => [...notes, newNote])
+        .then(updatedNotes => this.write(updatedNotes))
+        .then(() => this.newNote)
+    }
+    getNotes() {
+        return this.read()
+        .then(notes => {
+            return JSON.parse(notes)||[];
+        })
+    }
+    removeNote(id) {
+        return this.getNotes()
+        .then(notes => notes.filter(note => note.id !==id))
+        .then(keptNotes => this.write(keptNotes))
+    }
+}
+
+module.exports = new Store();
--- server.js
+++ server.js
@@ -6,75 +6,75 @@ const fs = require('fs');
 const util = require('util');
 const folderpath = '/public';
 
-    // Async Processes
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+//     // Async Processes
+// const readFileAsync = util.promisify(fs.readFile);
+// const writeFileAsync = util.promisify(fs.writeFile);
 
-    // Server Setup
-const app = express();
-const PORT = process.env.PORT || 3001;
+//     // Server Setup
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended:true}));
-app.use(express.json());
+// app.use(express.urlencoded({ extended:true}));
+// app.use(express.json());
 
-app.use(express.static(folderpath));
+// app.use(express.static(folderpath));
 
-    // API Routes
-    // GET Request
-app.get('/api/notes', function(req,res){
-    readFileAsync('./db/db.json', "utf8").then(function(data){
-        notes = [].concat(JSON.parse(data))
-        res.json(notes);
-    })
-});
+//     // API Routes
+//     // GET Request
+// app.get('/api/notes', function(req,res){
+//     readFileAsync('./db/db.json', "utf8").then(function(data){
+//         notes = [].concat(JSON.parse(data))
+//         res.json(notes);
+//     })
+// });
 
-    // POST Request/ Target 
+//     // POST Request/ Target 
 
-app.post("/api/notes", function(req,res){
-    const note = req.body;
-    readFileAsync("./db/db.json", "utfa8").then(function(data){
-        const notes = [].concat(json.parse(data));
-        note.id = notes.length + 1
-        notes.push(note);
-        return notes
-    }).then(function(notes){
-        writeFileAsync("./db/db.json", JSON.stringify(notes));
-        res.json(notes);
-    })
-});
+// app.post("/api/notes", function(req,res){
+//     const notes = req.body;
+//     readFileAsync("./db/db.json", "utfa8").then(function(data){
+//         const notes = [].concat(json.parse(data));
+//         note.id = notes.length + 1
+//         notes.push(note);
+//         return notes
+//     }).then(function(notes){
+//         writeFileAsync("./db/db.json", JSON.stringify(notes));
+//         res.json(notes);
+//     })
+// });
 
-    // Delete Request
+//     // Delete Request
 
-app.delete("/api/notes/:id", function(req,res){
-    const idDelete= parseInt(req.params.id);
-    readFileAsync("./db/db.json", "utf8").then(function(data){
-        const notes = [].concat(JSON.parse(data));
-        const newNoteData = []
-        for(let i = 0; i<notes.length; i++) {
-            if(idDelete !== notes[i].id) {
-                newNoteData.push(notes[i])
-            }
-        }
-        return newNoteData
-    }) .then(function(notes){
-        writeFileAsync("./db/db/json", JSON.stringify(notes))
-        res.send('Success');
-    })
-})
+// app.delete("/api/notes/:id", function(req,res){
+//     const idDelete= parseInt(req.params.id);
+//     readFileAsync("./db/db.json", "utf8").then(function(data){
+//         const notes = [].concat(JSON.parse(data));
+//         const newNotesData = []
+//         for(let i = 0; i<notes.length; i++) {
+//             if(idDelete !== notes[i].id) {
+//                 newNotesData.push(notes[i])
+//             }
+//         }
+//         return newNotesData
+//     }) .then(function(notes){
+//         writeFileAsync("./db/db/json", JSON.stringify(notes))
+//         res.send('Success');
+//     })
+// })
 
-    // HTML Routing
-app.get("/notes", function(req,res){
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-app.get("*", function(req, res){
-    res.sendFile(path.join(__dirname,"./public/index.html"));
-});
+//     // HTML Routing
+// app.get("/notes", function(req,res){
+//     res.sendFile(path.join(__dirname, "./public/notes.html"));
+// });
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
+// app.get("*", function(req, res){
+//     res.sendFile(path.join(__dirname,"./public/index.html"));
+// });
 
-    // Listen
+//     // Listen
 
-app.listen(PORT, function(){
-    console.log("App is working" + PORT);
-});
+// app.listen(PORT, function(){
+//     console.log("App is working" + PORT);
+// });
