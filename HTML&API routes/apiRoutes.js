0fb0d8a02be361004e4f912aff0bb2d87fb0ec93
macm <mac.mitsch@comcast.net>
2022-07-17 23:10:13 -0500

    trouble shooting api to post

--- HTML&API routes/apiRoutes.js
+++ HTML&API routes/apiRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const store = require("../db/store");
 
     // existing notes request
-router.get("/notes", function (req,res){
+router.get("/notes", function (req,res) {
     store
     .getNotes()
     .then(notes =>{ res.json(notes)
@@ -13,17 +13,16 @@ router.get("/notes", function (req,res){
 })
     // Posting new note
 
-router.post("/notes", function (req,res){
+router.post("/notes", function (req,res) {
     store
     .addNote(req.body)
     .then((note =>
         res.json(note))
-    .catch(err => {
-        res.status(500).json(err));
-    });
+        .catch(err => res.status(500).json(err))
+     });
     // Delete Note
 
-router.delete("/notes/:id", (req,res)=>{
+router.delete("/notes/:id", function (req,res) {
     store
     .removeNote(req.params.id)
     .then(()=> res.json({ok:true}))
