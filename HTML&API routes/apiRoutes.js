1e062bfe8ce6ce6cdb956942735ca1f4d107e9d6
macm <mac.mitsch@comcast.net>
2022-07-17 23:33:14 -0500

    work

--- HTML&API routes/apiRoutes.js
+++ HTML&API routes/apiRoutes.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const store = require("../db/store");
 
 
-router.get("/notes", function(req, res) {
+router.get("/api/notes", function(req, res) {
   store
     .getNotes()
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 });
 
-router.post("/notes", (req, res) => {
+router.post("/api/notes", (req, res) => {
   store
     .addNote(req.body)
     .then((note) => res.json(note))
