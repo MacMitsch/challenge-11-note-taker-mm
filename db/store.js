75dc362baada056a623bdae87d56725acb66a804
macm <mac.mitsch@comcast.net>
2022-07-17 22:39:09 -0500

    fixing store.js and trouble shooting new code

--- db/store.js
+++ db/store.js
@@ -1,38 +1,49 @@
 const util = require("util");
 const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
-const { parse } = require("path");
+const path = require("path");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store{
+    constructor(){
+        this.lastId= 0;
+    }
+    
+    
     read(){
         return readFileAsync("db/db.json", "utf8")
     }
     write(note) {
         return writeFileAsync("db/db.json", JSON.stringify(note))
     }
+   
+    getNotes() {
+        return this.read()
+        .then(notes => {
+            let parsedNotes;
+            try{ parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) { parsedNotes =[];
+            }
+            return parsedNotes
+        });
+    }
+
     addNote(note){
+        console.log("made it past store")
         const { title, text } = note
         if(!title|| !text){
             throw new Error("error please fill title and text.")
         }
         const newNote = {title,text,id:uuidv4()}
         
-console.log("made it past store")
+
 
         return this.getNotes()
         .then(notes => [...notes, newNote])
         .then(updatedNotes => this.write(updatedNotes))
         .then(() => this.newNote)
     }
-    getNotes() { console.log("get notes initiated")
-        return this.read()
-        .then(notes => {
-            return JSON.parse(notes)||[];
-        })
-        }
 
     
 
