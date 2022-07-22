8967ed658c2951d447c71d0e08fb276a4dfa5168
macm <mac.mitsch@comcast.net>
2022-07-21 22:14:29 -0500

    troubleshooting my lovely code

--- HTML&API routes/apiRoutes.js
+++ HTML&API routes/apiRoutes.js
@@ -1,27 +1,34 @@
 const express = require("express");
 const router = require("express").Router();
 const store = require("./../db/store");
+const fs = require("fs");
 
 
 // get
 router.get("/notes", (req, res) => {
- store.getNote()
-  .then(note => res.json(note))
-  .catch(err => res.status(400).json(err))
+ store
+  .getNotes()
+  // .then(note => res.json(note))
+  // console.log("note",note)
+  // .catch(err => res.status(400).json(err))
+
 });
 
 router.post('/notes', (req, res) => {
   // set id 
-  store.addNote(req.body)
-    .then((note) => res.json(note))
-    .catch(err => res.status(400).json(err))
+  console.log("rec body",req.body)
+  store
+  .addNote(req.body)
+    // .then((note) => res.json(note))
+    // .catch(err => res.status(400).json(err))
 });
 
 // Route
 router .delete('/notes/:id', (req, res) => {
-  store .deleteNote(req.params.title)
-    .then(()=> res.json({ok:true}))
-    .catch(err => res.status(400).json(err))
+  store 
+  .deleteNote(req.params.title)
+    // .then(()=> res.json({ok:true}))
+    // .catch(err => res.status(400).json(err))
 });
 
 module.exports = router;
--- db/store.js
+++ db/store.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const util = require("util");
 const express = require("express");
+const res = require('express/lib/response');
 const app = express();
 
 
@@ -18,27 +19,32 @@ class Store {
   };
 
   write(note){
-    return writeFileAsync(path.join(__dirname, "db.json"), JSON.stringify(note));
+    return writeFileAsync(path.join(__dirname, "db.json"));
   };
 
-  getNote(){
-    return this.read().then(note =>{
-      let parsedNote = Json.parse(note);
-      console.log(parsedNote);
+  getNotes(){
+     this.read().then(note =>{
+      console.log("im in the getnote", note)
+      let parsedNote = JSON.parse(note);
+      console.log("parsenote is",parsedNote);
       return parsedNote;
     });
   };
 
   addNote(newNote) {
-    console.log(newNote);
-    return this.getNote().then(note => {
-      const newNoteList = [...note,newNote];
-      console.log(newNoteList);
-    })
+    
+    this.write(newNote)
+    console.log("test add Note",newNote);
+    return this.getNotes()
+    //   .then(newNote => {
+    //   console.log("what the hell is", note)
+    //   const newNoteList = [...note,newNote];
+    //   console.log("this is the note list",newNoteList);
+    // })
   };
   
 deleteNote(title) {
-  return this.getNote()
+  return this.getNotes()
     .then(note => {
       console.log(title);
       for(var i=0; i<note.length; i++){
